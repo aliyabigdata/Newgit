6fb86d915d28c7ca77cb54152284b05027530cb1
aliyabigdata <aliya.bermaganbet@alumni.nu.edu.kz>
2022-02-12 21:50:07 +0300

    I included new commits

--- mygit.c
+++ mygit.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+void Aliya()
+{
+ printf("I learned how to work with git!");
+ return 0;
+}
 int main()
 {
  int a = 5;
  int b = 10;
- return a + b;
+ printf("%d",a);
+ printf("%d",b);
+ Aliya();
+ return 0;
 }
